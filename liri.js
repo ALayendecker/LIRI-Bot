641e8944d1a39a2363f06f6144f257c8a2c8f9fd
ALayendecker <alcommits@gmail.com>
2019-10-10 13:21:09 -0500

    minor pseudo code liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,47 @@
 require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+switch (key) {
+  case "concert-this":
+    // make function that does
+
+    break;
+  case "spotify-this-song":
+    // make function that does
+    break;
+  case "movie-this":
+    // make function that does
+    break;
+  case "do-what-it-says":
+    // make function that does
+    break;
+
+  default:
+    break;
+}
+//=========
+
+// Function for   case "concert-this":
+
+//=========
+
+//=========
+
+// Function for case "spotify-this-song":
+
+//=========
+
+//=========
+
+// Function for case "movie-this":
+
+//=========
+
+//=========
+
+// Function for  case "do-what-it-says":
+
+//=========
