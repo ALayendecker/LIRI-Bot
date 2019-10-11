0da551e4d39ba287e95d07a3a6e4946eabf0a647
ALayendecker <alcommits@gmail.com>
2019-10-11 14:30:14 -0500

    completed movie this and concert this

--- liri.js
+++ liri.js
@@ -1,47 +1,141 @@
+//==============================
+//JUNK
+
 require("dotenv").config();
-
 var keys = require("./keys.js");
-
+var moment = require("moment");
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-switch (key) {
+var input = process.argv[2];
+//===============================
+
+switch (input) {
   case "concert-this":
+    bandsInTown();
     // make function that does
 
     break;
   case "spotify-this-song":
+    spotify();
     // make function that does
+
     break;
   case "movie-this":
+    movie();
     // make function that does
+
     break;
   case "do-what-it-says":
+    doWhat();
     // make function that does
+
     break;
 
   default:
     break;
 }
-//=========
+//=================================================================================
 
 // Function for   case "concert-this":
+function bandsInTown() {
+  var artist = process.argv[3];
+  var queryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
+  axios.get(queryUrl).then(function(response) {
+    console.log(`
+        Venue: ${response.data[0].venue.name}
+        Location ${
+          response.data[0].venue.region
+        } + ${response.data[0].venue.city}
+        Date: ${moment(response.data[0].datetime).format("MM/DD/YYYY")}
+        `);
+  });
+}
+//=================================================================================
 
-//=========
-
-//=========
+//=================================================================================
 
 // Function for case "spotify-this-song":
+function spotify() {
+  Spotify;
+  var songName = process.argv[3];
+  spotify
+    .search({ type: "track", query: songName })
+    .then(function(response) {
+      console.log(`
+        Artist(s): ${response.data[0].venue.name}
+        Song Name ${response.data[0].venue.region}
+        Preview: ${moment(response.data[0].datetime).format("MM/DD/YYYY")}
+        Album: ${response.data[0].venue.region}
+        `);
+      if (process.argv[3] === "") {
+        console.log("No song detected try this.");
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
 
-//=========
+//=================================================================================
 
-//=========
+//=================================================================================
 
 // Function for case "movie-this":
+//======================================================================================
+function movie() {
+  var movieName = process.argv[3];
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      console.log(`
+        Movie Title: ${response.data.Title}
+        Release Year: ${response.data.Release}
+        IMDB Rating: ${response.data.imdbRating}
+        Rotten Tomatoes Rating: ${response.data.Ratings[1].value}
+        Country Produced: ${response.data.Country}
+        Language: ${response.data.Language}
+        Plot: ${response.data.Plot}
+        Actors: ${response.data.Actors}
+        `);
+    })
+    .catch(function(error) {
+      if (error.response) {
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 
-//=========
+  if (process.argv[3] === "") {
+    console.log(
+      "If you haven't watched 'Mr.Nobody,' then you should: http://www.imdb.com/title/tt0485947/" +
+        "It's on Netflix"
+    );
+  }
+}
 
-//=========
+//=================================================================================
+
+//=================================================================================
 
 // Function for  case "do-what-it-says":
 
-//=========
+//=================================================================================
